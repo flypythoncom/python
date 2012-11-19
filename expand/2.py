603b8ca0145f3be510ebc594c6bd1249b7f25f1e
Jimmy <xxg1413@gmail.com>
2012-11-19 21:16:17 +0800

    new file:   expand/2.py

---
+++ expand/2.py
@@ -0,0 +1,27 @@
+﻿#s = raw_input()
+#i = 0
+#for i in s:
+#    print i
+#raw_input()
+
+#扩展成在字符串中把特定字符替换成规定字符
+import re
+def printReplace(srcStr,word):
+    for letter in srcStr:
+        if letter == word:
+            srcStr = srcStr.replace(letter,'!') #字符串不可更改
+    return srcStr
+
+def printReplace_re(srcStr,word):
+    srcStr=re.sub(word,'!',srcStr)
+    return srcStr
+
+def main():    
+    srcStr = raw_input(u"请输入一个字符串：")
+    destStr=printReplace_re(srcStr,'i')
+    print destStr
+
+
+if __name__ == "__main__":
+    main()
+            
