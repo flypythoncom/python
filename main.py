567eefba7cc25159fbf93c3ac9f050a6bb03463f
c00lhawk607 <57409539+c00lhawk607@users.noreply.github.com>
2020-10-05 14:36:27 -0500

    Update main.py
    Opens youtube link in browser

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 import os
 
+
+#Opens youtube link in browser.
 if os.name == "nt":
 	try:
-		os.system("start chrome")
+		os.system("start chrome https://www.youtube.com/watch?v=_uQrJ0TkZlc&list=PLTjRvDozrdlxj5wgH4qkvwSOdHLOCx10f")
 	except:
-		os.system("start iexplore")
+		os.system("start iexplore https://www.youtube.com/watch?v=_uQrJ0TkZlc&list=PLTjRvDozrdlxj5wgH4qkvwSOdHLOCx10f")
 else:
 	try:
-		os.system("firefox")
+		os.system("firefox https://www.youtube.com/watch?v=_uQrJ0TkZlc&list=PLTjRvDozrdlxj5wgH4qkvwSOdHLOCx10f")
 	except:
-		os.system("open -a Safari")
+		os.system("open -a Safari https://www.youtube.com/watch?v=_uQrJ0TkZlc&list=PLTjRvDozrdlxj5wgH4qkvwSOdHLOCx10f")
